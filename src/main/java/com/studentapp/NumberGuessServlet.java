bf1e0bf475c812116562b5e8e44f4ef047c52851
omotomiwa26 <omotomiwa26@gmail.com>
2025-09-05 01:57:13 +0100

    FEAT: Add getTargetNumber method to retrieve the current target number

--- src/main/java/com/studentapp/NumberGuessServlet.java
+++ src/main/java/com/studentapp/NumberGuessServlet.java
@@ -8,6 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+public int getTargetNumber() {
+    return targetNumber;
+}
+
 public class NumberGuessServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private int targetNumber;
